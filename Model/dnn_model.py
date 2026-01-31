d13708be5c998d8f4652eee29236d4c5f9f1b477
Pranavan-S <subendiranp@gmail.com>
2026-01-31 14:56:27 +0530

    Add DNN model definition

---
+++ Model/dnn_model.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.utils.data import DataLoader
+
+class MLP(nn.Module):
+    def __init__(self, in_dim:int, 
+                 hidden_dim:tuple=(256, 128, 64, 32), 
+                 drop_out:tuple=(0.1, 0.1, 0.1, 0.1), 
+                 out_dim:int=4
+        ):
+        super().__init__()
+
+
+        layers_stack = []
+        n_node_prev = in_dim
+
+        for h, d in zip(hidden_dim, drop_out):
+
+            # add linear layer
+            layers_stack.append(nn.Linear(n_node_prev, h))
+
+            # add batch normalization layer
+            layers_stack.append(nn.BatchNorm1d(h))
+
+            # add ReLU activation
+            layers_stack.append(nn.ReLU())
+            
+            # add dropout
+            layers_stack.append(nn.Dropout(d))
+
+            n_node_prev = h
+
+        # output layer
+        layers_stack.append(nn.Linear(n_node_prev, out_dim))
+
+
+        # model architecture
+        self.net = nn.Sequential(*layers_stack)
+    
+    def forward(self, x):
+        return self.net(x)
