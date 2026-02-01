0b2a4185777b263c58807bd12410ec6da8b01c52
Pranavan-S <subendiranp@gmail.com>
2026-02-01 11:43:33 +0530

    remove unused imports from model_definition.py

--- Model/model_definition.py
+++ Model/model_definition.py
@@ -1,11 +1,4 @@
-import pandas as pd
-import numpy as np
-
-import torch
 import torch.nn as nn
-import torch.optim as optim
-
-from torch.utils.data import DataLoader
 
 class MLP(nn.Module):
     def __init__(self, in_dim:int, 
