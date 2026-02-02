f0ead6da5d7dcb77c147ef4978f186413d3f7f7e
Pranavan-S <subendiranp@gmail.com>
2026-02-02 14:00:19 +0530

    fix: correct numpy copy behavior in TabularDataset initialization

--- Model/tabular_dataset.py
+++ Model/tabular_dataset.py
@@ -5,11 +5,11 @@ from torch.utils.data import Dataset
 class TabularDataset(Dataset):
     def __init__(self, X_df, y_df):
         # pytorch expects float32 for dnn modeling
-        X = X_df.to_numpy(dtype=np.float32, copy=False)
-        y = y_df.to_numpy(dtype=np.float32, copy=False) # Removed .reshape(-1, 1) for multi-label
+        X = X_df.to_numpy(dtype=np.float32, copy=True)
+        y = y_df.to_numpy(dtype=np.float32, copy=True)
 
         # loads data into self.X, self.y without copying: memory efficient
-        # i.e. uses the same object created in the memory, just points to the location instead of creating another copy of X, y
+        # uses the same object created in the memory, just points to the location instead of creating another copy of X, y
         self.X = torch.from_numpy(X)
         self.y = torch.from_numpy(y)
 
