fix: update scaling logic to remove log transformation and handle negative values

Model/scale_numeric_features.py
@@ -9,20 +9,12 @@ def scale_and_log_transform(df: pd.DataFrame, train_scaler: Optional[StandardSca
 
     # get numeric features
     df_numeric = df[NUMERIC_COLS]
-
-    for col in NUMERIC_COLS:
-        assert df_numeric[col].min() >= 0, "Negative values provided for log transform"
-
-    # apply log transform
-    df_log = pd.DataFrame()
-    df_log[NUMERIC_COLS] = np.log1p(df_numeric[NUMERIC_COLS])
-
     
     # train scaler is not there (train_df)
     if not train_scaler:
-        train_scaler = StandardScaler().set_output(transform="pandas").fit(df_log)
+        train_scaler = StandardScaler().set_output(transform="pandas").fit(df_numeric)
 
-    scaled_df = pd.DataFrame(train_scaler.transform(df_log))
+    scaled_df = pd.DataFrame(train_scaler.transform(df_numeric))
 
     for col in NUMERIC_COLS:
         df[col] = scaled_df[col]